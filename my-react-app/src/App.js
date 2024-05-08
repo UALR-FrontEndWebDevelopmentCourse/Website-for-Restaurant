6bbd96d954b8ee53ed0fbddfcc4212fb214f65be
Lezly Luna <89171673+LezlyLuna@users.noreply.github.com>
2024-05-07 20:00:31 -0500

    Main stuff
    adding some content

---
+++ my-react-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+            <h1>Meta Frontend Developer Capstone Starter Files</h1>
+
+    </div>
+  );
+}
+
+export default App;
