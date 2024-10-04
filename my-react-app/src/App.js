1232e7c90c28ea71b74e9edeb7a7ffe07181880d
Lezly Luna <lluna@ualr.edu>
2024-10-03 19:35:22 -0500

    Revert "Finished what I could"
    This reverts commit 29907b0916bf4715f9234d032b9e584734f15f3c.

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-import Nav from './components/Nav';
-import Main from './components/Main';
-import Menu from './components/Menu';
-import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-    <Nav/>
-    <Main/>
-    <Menu/>
-    <Footer/>
+    <div className="App">
+            <h1>Meta Frontend Developer Capstone Starter Files</h1>
 
-    {/*<Header/>
-    <BookingForm/>*/}
-    </>
+    </div>
   );
 }
 
